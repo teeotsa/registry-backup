aee219ec7d11a5997e8ec370b920e0c0ccac94fa
Teeotsa <78772453+teeotsa@users.noreply.github.com>
2022-01-01 17:02:53 +0200

    Add files via upload

---
+++ Registry Restore Apply.ps1
@@ -0,0 +1,9 @@
+﻿Clear
+$ErrorActionPreference = "SilentlyContinue"
+$RestoreFolder = "$PSScriptRoot\Restore"
+
+Get-ChildItem -Path $RestoreFolder | ForEach-Object{
+        
+    reg import $_.FullName
+
+}
